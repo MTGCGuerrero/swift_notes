f6d3fe7775418bc1cc8a75f2654e9bd794955d2c
MacanMichis <mtgcenturion@hotmail.com>
2023-03-06 16:30:58 -0300

    initial commit

---
+++ notes_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notes_app
+//
+//  Created by MaCanMichis on 3/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notes_app/notes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_appApp.swift
+//  notes_app
+//
+//  Created by MaCanMichis on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct notes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
