6edd6b3875197c442ecbe18cb3edcc799585e9b2
MacanMichis <mtgcenturion@hotmail.com>
2023-03-15 16:28:21 -0300

    delete and update ui

--- notes_app/Home.swift
+++ notes_app/Home.swift
@@ -10,13 +10,27 @@ import SwiftUI
 struct Home: View {
     @State var notes = [Note]()
     @State var showAdd = false
+    @State var showAlert = false
+    @State var deleteItem: Note?
+    var alert: Alert{
+        Alert(title: Text("Delete"), message: Text("Are you sure you want to delete this note"), primaryButton: .destructive(Text("Delete"),action: deleteNote), secondaryButton: .cancel())
+    }
     var body: some View {
         
         NavigationView {
             List(self.notes){ note in
                 Text(note.note).padding()
+                    .onLongPressGesture {
+                        self.showAlert.toggle()
+                        deleteItem = note
+                    }
             }
-            .sheet(isPresented: $showAdd, content: {
+            .alert(isPresented: $showAlert, content: {
+                alert
+            })
+            .sheet(isPresented: $showAdd, onDismiss: {
+                fetchNotes()
+            }, content: {
                 AddNoteView()
             })
             .onAppear(perform: {
@@ -36,6 +50,30 @@ struct Home: View {
         
         
     }
+    
+    func deleteNote(){
+        guard let id = deleteItem?._id else {return}
+        let url = URL(string: "http://localhost:3000/\(id)")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        let task = URLSession.shared.dataTask(with: request){ data,res,err in
+            guard err == nil else {return}
+            
+            guard let data = data else {return}
+            do{
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any]{
+                    print(json)
+                }
+            }
+            catch let error{
+                print(error)
+            }
+        }
+        task.resume()
+        print("Delete")
+        fetchNotes()
+    }
+    
     func fetchNotes(){
         let url = URL(string: "http://localhost:3000/notes")!
         let task = URLSession.shared.dataTask(with: url){ data,res,err in
