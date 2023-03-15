2d228aa35d3677a9312c4f29288a681e1ea24dfc
MacanMichis <mtgcenturion@hotmail.com>
2023-03-15 13:37:51 -0300

    add new view

---
+++ notes_app/AddNoteView.swift
@@ -0,0 +1,20 @@
+//
+//  AddNoteView.swift
+//  notes_app
+//
+//  Created by MaCanMichis on 3/15/23.
+//
+
+import SwiftUI
+
+struct AddNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddNoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNoteView()
+    }
+}
